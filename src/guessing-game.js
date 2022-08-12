9557b4f0fce148ddbc920c67cc9e010d58d36faf
alyamba <logunionok@gmail.com>
2022-08-12 14:28:05 +0300

    feat: add functional

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,29 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.min = null;
+    this.max = null;
+    this.pivot = null;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    this.pivot = Math.round((this.min + this.max) / 2);
+    return this.pivot;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.pivot;
+    this.guess();
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.pivot;
+    this.guess();
+  }
 }
 
 module.exports = GuessingGame;
